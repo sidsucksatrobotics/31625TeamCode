74a54b870874f0fd908e61bfc6a078e3450387d4
sidsucksatrobotics <siddhant.p@mckinneysteamacademy.org>
2025-09-21 17:50:41 -0500

    i edited the the teleop to include the code for the shooter, both motors and servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -2,15 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="Mecanum Drive with Toggle Precision Mode", group="Linear Opmode")
-public class MecanumDrive extends LinearOpMode {
+@TeleOp(name="TeleOp Drive with Toggle Precision Mode", group="Linear Opmode")
+public class TeleOpDrive extends LinearOpMode {
 
-    private DcMotor leftFront, leftRear, rightFront, rightRear;
+    private DcMotor leftFront, leftRear, rightFront, rightRear, mainShooter;
+    private CRServo leftShooter, rightShooter;
 
     private boolean precisionMode = false; // starts full speed
-    private boolean lastBumperState = false; // for detecting button press
+    private boolean lastBumperState = false; // for detecting button press of slow mode
+    private boolean shooterMode = false; // starts with shooters off
+    private boolean lastShooterState = false; // for detecting button press of shooter
 
     @Override
     public void runOpMode() {
@@ -19,14 +23,16 @@ public class MecanumDrive extends LinearOpMode {
         leftRear   = hardwareMap.get(DcMotor.class, "leftRear");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         rightRear  = hardwareMap.get(DcMotor.class, "rightRear");
+        leftShooter = hardwareMap.get(CRServo.class, "leftShooter");
+        rightShooter = hardwareMap.get(CRServo.class, "rightShooter");
 
         // Reverse right side motors
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         rightRear.setDirection(DcMotor.Direction.REVERSE);
-
+        // Report initialized status
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
+        // Wait to start
         waitForStart();
 
         while (opModeIsActive()) {
@@ -35,6 +41,12 @@ public class MecanumDrive extends LinearOpMode {
                 precisionMode = !precisionMode;
             }
             lastBumperState = gamepad1.left_bumper;
+
+            // Toggle shooter mode on button press of "A"
+            if (gamepad1.a && !lastShooterState) {
+                shooterMode=!shooterMode;
+            }
+            lastShooterState = gamepad1.a;
 
             // Joystick inputs
             double y  = -gamepad1.left_stick_y;  // forward/back
@@ -67,6 +79,12 @@ public class MecanumDrive extends LinearOpMode {
                 backRightPower  *= 0.5;
             }
 
+            if (shooterMode) {
+                mainShooter.setPower(-1);
+                leftShooter.setPower(-1);
+                rightShooter.setPower(1);
+            }
+
             // Set motor powers
             leftFront.setPower(frontLeftPower);
             leftRear.setPower(backLeftPower);
@@ -74,6 +92,7 @@ public class MecanumDrive extends LinearOpMode {
             rightRear.setPower(backRightPower);
 
             telemetry.addData("Mode", precisionMode ? "Precision (Half Speed)" : "Full Speed");
+            telemetry.addData("Mode", shooterMode ? "Shooter" + "On" : "Off");
             telemetry.addData("Front Left", frontLeftPower);
             telemetry.addData("Back Left", backLeftPower);
             telemetry.addData("Front Right", frontRightPower);
